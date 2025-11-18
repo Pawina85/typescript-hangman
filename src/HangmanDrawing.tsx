f43c5f708175c00aaa5668e0f0857118ff7ed3a8
Pawina85 <pawinachanthachon@gmail.com>
2025-11-18 15:55:57 +0700

    Initial commit

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,12 @@
+
+
+export function HangmanDrawing() {
+   return (
+   <div style={{ position: "relative"}}>
+    <div style={{ height: "10px", width: "250px", background: "black"}}>
+
+    </div>
+
+   </div>
+   )
+}
